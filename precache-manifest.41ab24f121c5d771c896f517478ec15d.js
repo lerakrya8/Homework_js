ed07482bd39b514f09dc14c625291debbed98ae0
lerakrya8 <lerakrya8@gmail.com>
2020-05-27 21:54:15 +0300

    Updates

---
+++ precache-manifest.41ab24f121c5d771c896f517478ec15d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d465d26620808bb36c6e0adada20450",
+    "url": "/js_homework/index.html"
+  },
+  {
+    "revision": "bbf6399f80f8953018ae",
+    "url": "/js_homework/static/css/main.a987e4fd.chunk.css"
+  },
+  {
+    "revision": "c31028735238355ef65f",
+    "url": "/js_homework/static/js/2.5fd5bdaa.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/js_homework/static/js/2.5fd5bdaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf6399f80f8953018ae",
+    "url": "/js_homework/static/js/main.8e5e7268.chunk.js"
+  },
+  {
+    "revision": "9b0bf7e9010045e51c7a",
+    "url": "/js_homework/static/js/runtime-main.d156edc2.js"
+  },
+  {
+    "revision": "cccdc452b447c69944a0b5239b71b2d3",
+    "url": "/js_homework/static/media/Beautiful-nature-landscape-mountains-top-view-clouds-fog-morning_2560x1600.cccdc452.jpg"
+  },
+  {
+    "revision": "ba80d2b3112f3142f67ee256487c8988",
+    "url": "/js_homework/static/media/unnamed.ba80d2b3.png"
+  }
+]);
